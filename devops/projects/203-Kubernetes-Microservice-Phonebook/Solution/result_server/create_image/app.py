f78b71fecb46221ee86df5b72c14e13de9736fac
Anthny <anthnycara@gmail.com>
2020-12-12 16:54:26 +0300

    Kubernetes 203

--- devops/projects/203-Kubernetes-Microservice-Phonebook/Solution/result_server/create_image/app.py
+++ devops/projects/203-Kubernetes-Microservice-Phonebook/Solution/result_server/create_image/app.py
@@ -48,5 +48,5 @@ def find_records():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__== '__main__':
-    #app.run(debug=True)
+       #app.run(debug=True)
     app.run(host='0.0.0.0', port=80) 
